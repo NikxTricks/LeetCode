73acdd8c42e249b650f574a86e8482ab2c140383
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-07-31 16:28:46 -0400

    Time: 3 ms (18.33%), Space: 45.2 MB (62.89%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,29 @@
+
 class Solution {
     public void moveZeroes(int[] nums) {
-        int nextNonZeroPos = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[nextNonZeroPos] = nums[i];
-                nextNonZeroPos++;
+        int skipped = 0;
+        int start = 0;
+        while (start < nums.length && nums[start] != 0) {
+            start++;   
+        }
+        int j = start;
+        for (int i = start; i < nums.length-skipped; i++) {
+            while (j < nums.length && nums[j] == 0) {
+                skipped++;
+                j++;
             }
-        }
-        for (int i = nextNonZeroPos; i < nums.length; i++) {
-            nums[i] = 0;
-        }
+            if (j < nums.length) {
+                nums[i] = nums[j];
+                nums[j] = 0;
+                j++; 
+            }
+
+        } 
+        
+        
+
+        
+        
+
     }
 }
