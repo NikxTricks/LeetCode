cf58668c18c489057d54b7d0c966a2bbed989efb
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-02 17:18:46 -0400

    Time: 0 ms (100.00%), Space: 44.5 MB (27.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,16 +2,15 @@ import java.util.Set;
 import java.util.Arrays;
 class Solution {
     public int missingNumber(int[] nums) {
-        HashSet<Integer> data = new HashSet<>();
+        int sum = 0;
+        for (int i = 1; i<= nums.length; i++) {
+            sum += i;
+        }
         for (int num: nums) {
-            data.add(num);
+            sum-= num;
         }
         
-        for (int i = 0; i<= nums.length; i++) {
-           if (!data.contains(i)) {
-               return i;
-           } 
-        }
-        return -1;
+
+        return sum;
     }
 }
