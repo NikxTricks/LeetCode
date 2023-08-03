90b83ab6864149c2640f8da223e07c5116f1d399
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-03 19:13:15 -0400

    Time: 10 ms (32.59%), Space: 57.2 MB (48.44%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,50 +1,28 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
-import java.util.Stack;
-
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        
+
         ListNode cur = head;
-        int size = 0;
-        Stack<Integer> reverse = new Stack<>();
-        
+
+        Deque<Integer> data = new ArrayDeque<>();
+
+
         while (cur != null) {
-            size++;
+            data.addLast(cur.val);
             cur = cur.next;
         }
-        
-        int mid = size/2;
-        
-        cur = head;
-        for (int i = 0;i < mid; i++) {
-            reverse.push(cur.val);
-            cur = cur.next;
-        }
-        
-        if (size%2==1) {
-            cur = cur.next;
-        }
-        
-        while (cur != null) {
-            if (reverse.pop() != cur.val) {
-              return false;  
+
+        while (data.size() != 0) {
+            if (data.size() == 1) {
+                return true;
             }
-            cur = cur.next;
+            if (data.removeFirst() != data.removeLast()) {
+                return false;
+                
+                            }
+           
         }
-        
-        
+
+
         return true;
-        
-        
-    }
+            }
 }
