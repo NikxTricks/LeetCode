0c48cf31a02236beaa681d17efe8c68c8c042e43
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-04 16:49:07 -0400

    Time: 12 ms (34.80%), Space: 43.5 MB (62.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,34 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> data = new HashMap<>();
-        String big = "";
-        String small = "";
-        if (s.length() > t.length()) {
-            big = s;
-            small = t;
-        }
-        else {
-            big = t;
-            small = s;
-        }
-        for (int i = 0; i < small.length(); i++) {
-            if (data.containsKey(small.charAt(i))) {
-                data.put(small.charAt(i), data.get(small.charAt(i)) + 1);
-            }
-            else {
-                data.put(small.charAt(i), 1);
-            }
+        if (s.length() != t.length()) {
+            return false;
         }
         
-        for (int i = 0; i < big.length(); i++) {
-            if (!data.containsKey(big.charAt(i))) {
+        int[] sarr = new int[s.length()];
+        int[] tarr = new int[s.length()];
+        
+        for (int i = 0; i < s.length(); i++) {
+            sarr[i] = (int) s.charAt(i);
+            tarr[i] = (int) t.charAt(i);
+        }
+        
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (sarr[i] != tarr[i]) {
                 return false;
-            }
-            else if (data.get(big.charAt(i)) == 0) {
-                return false;
-            }
-            else {
-                data.put(big.charAt(i), data.get(big.charAt(i)) - 1);
             }
         }
         return true;
