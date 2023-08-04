0340142f087bf08f22c502054e0df0dfd9cd29a0
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-04 17:28:23 -0400

    Time: 5 ms (58.88%), Space: 41.8 MB (96.60%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] data = new int[26];
+
         if (s.length() != t.length()) {
             return false;
         }
+        int[] data = new int[26];
         
         for (int i = 0; i < s.length(); i++) {
             data[(s.charAt(i)) - 'a']++;
