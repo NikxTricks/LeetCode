0f33757b5cef90389676c6d2982667d4160773bb
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-05 18:32:09 -0400

    Time: 0 ms (100.00%), Space: 43.6 MB (67.68%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,7 +13,7 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode cur1 = head;
         ListNode cur2 = head;
-        while (cur2 != null && cur2.next != null && cur2.next.next != null) {
+        while (cur2 != null && cur2.next != null) {
             cur1 = cur1.next;
             cur2 = cur2.next.next;
             if (cur1 == cur2) {
