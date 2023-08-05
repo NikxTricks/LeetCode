48264fef0393fa1920a1dfed10c83fe975c08c9b
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-05 14:49:11 -0400

    Time: 0 ms (100.00%), Space: 40.7 MB (76.62%) - LeetHub

---
+++ 0163-missing-ranges/0163-missing-ranges.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
+        ArrayList<List<Integer>> out = new ArrayList<>();
+        LinkedList<Integer> bucket = new LinkedList<>();
+        if (nums.length == 0) {
+            bucket.add(lower);
+            bucket.add(upper);
+            out.add(bucket);
+            return out;
+        }
+        if (lower + 1 <= nums[0]) {
+            bucket.add(lower);
+            bucket.add(nums[0] - 1);
+            out.add(bucket);
+            bucket = new LinkedList<>();
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] + 1 != nums[i + 1]) {
+                bucket.add(nums[i] + 1);
+                bucket.add(nums[i + 1] - 1);
+                out.add(bucket);
+                bucket = new LinkedList<>();
+            }
+        }
+        if (nums[nums.length - 1] + 1 <= upper) {
+            bucket.add(nums[nums.length - 1] + 1);
+            bucket.add(upper);
+            out.add(bucket);
+        }
+        return out;
+    }
+}
