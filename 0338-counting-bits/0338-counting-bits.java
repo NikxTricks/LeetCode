2c3b9f98bc293035ba4ed2121468d60d6e321ad5
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-05 21:57:44 -0400

    Time: 3 ms (36.35%), Space: 46.6 MB (30.44%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,19 +3,13 @@ class Solution {
         int[] out = new int[n + 1];
         int count = 0;
         int place = 0;
-        int prev = 1;
-        int track = 2;
         for (int i = 0; i < n + 1; i++) {
             count = 0;
             for (int j = 0; j < 32; j++) {
                 count+= (i>>j) & 1;
             }
             out[i] = count;
-            track--;
-            if (track == 0) {
-               prev *= 2;
-                track = prev;
-            }
+            //place *= 2;
         }
         
         return out;
