1b170b2f7c03eb15c2f987054757a91c660cd053
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-05 21:57:53 -0400

    Time: 3 ms (36.35%), Space: 47 MB (9.05%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,14 +2,12 @@ class Solution {
     public int[] countBits(int n) {
         int[] out = new int[n + 1];
         int count = 0;
-        int place = 0;
         for (int i = 0; i < n + 1; i++) {
             count = 0;
             for (int j = 0; j < 32; j++) {
                 count+= (i>>j) & 1;
             }
             out[i] = count;
-            //place *= 2;
         }
         
         return out;
