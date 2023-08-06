4ad21067b4c4ec139af0b49ec6697c8746f1ef26
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-05 22:04:03 -0400

    Time: 4 ms (32.12%), Space: 46.4 MB (63.54%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,7 +1,12 @@
 class Solution {
     public int[] countBits(int n) {
         int[] out = new int[n + 1];
+        int max = 1;
         for (int i = 0; i < n + 1; i++) {
+            if (i%2 == 1) {
+                    out[i] = out[i - 1] + 1;
+                    continue;
+            }
             for (int j = 0; j < 32; j++) {
                 out[i]+= (i>>j) & 1;
             }
