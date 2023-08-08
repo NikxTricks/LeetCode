445019f771190a23808e88f8d3d1bec6a541223b
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-08 18:27:29 -0400

    Time: 2 ms (90.49%), Space: 40.4 MB (88.98%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length()%2 != 0) {
+            return false;
+        }
+        
+        Stack<Character> reverse = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                reverse.push(')');
+                
+            }
+            else if (s.charAt(i) == '[') {
+                reverse.push(']');
+                
+            }
+            else if (s.charAt(i) == '{') {
+                reverse.push('}');
+                
+            }
+            else if (reverse.size() == 0 || reverse.pop() != s.charAt(i)) {
+                return false;
+            }
+        }
+        if (reverse.size() != 0) {
+            return false;
+        }
+        return true;
+    }
+}
