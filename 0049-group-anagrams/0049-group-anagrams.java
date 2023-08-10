8a47e675f29314d79fff43ab2efb96545c50e27c
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-10 17:11:06 -0400

    Time: 46 ms (10.54%), Space: 48.1 MB (15.46%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        HashMap<String, List<String>> data = new HashMap<>();
+        List<List<String>> out = new LinkedList<>();
+        List<String> temp;
+        int[] chars;
+        String sorted = "";
+        
+        for (String str: strs) {
+            sorted = "";
+            chars = new int[str.length()];
+            for (int i = 0; i < str.length(); i++) {
+                chars[i] = (int) str.charAt(i);
+            }
+            Arrays.sort(chars);
+            for (int c: chars) {
+                sorted = sorted + ((char) c);
+            }
+            System.out.println(sorted);
+            if (data.containsKey(sorted)) {
+                data.get(sorted).add(str);
+            }
+            else {
+                temp = new LinkedList<>();
+                temp.add(str);
+                data.put(sorted,temp);
+            }
+        }
+        
+        for (String key: data.keySet()) {
+            out.add(data.get(key));
+        }
+        
+        return out;
+    }
+}
