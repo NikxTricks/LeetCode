57acc72a815eac301d7e83591fed158fef03c929
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-11 18:26:49 -0400

    Time: 13 ms (7.02%), Space: 46.1 MB (8.03%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,29 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer, Integer> data = new HashMap<>();
+        //HashMap<Integer, Integer> data = new HashMap<>();
         int[] out = new int[2];
         for (int i = 0; i < numbers.length; i++) {
-            if (data.containsKey(target - numbers[i])) {
-                out[0]  = Math.min(i, data.get(target - numbers[i])) + 1;
-                out[1] = Math.max(i, data.get(target - numbers[i])) + 1;
-                return out;
-            }
-            else {
-                data.put(numbers[i], i);
+            int temp = Math.max(binarySearch(numbers, target - numbers[i], 0,i), binarySearch(numbers, target - numbers[i], i + 1, numbers.length - 1));
+            if (temp != -1) {
+                out[0] = Math.min(temp, i) + 1;
+                out[1] = Math.max(temp, i) + 1;
             }
         }
         
         return out;
+    }
+    
+    public int binarySearch(int[] numbers, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        int mid = (start + end)/2;
+        if (numbers[mid] == target) {
+            return mid;
+        }
+       if (numbers[mid] > target) {
+            return binarySearch(numbers, target, start, mid - 1);
+        }
+        return binarySearch(numbers, target, mid + 1, end);
     }
 }
