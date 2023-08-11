27eaf04c7277197184fcf2ff0ab7ec00dbc1320d
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-11 18:55:07 -0400

    Time: 2 ms (44.30%), Space: 45.3 MB (78.88%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,33 +1,25 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > target - numbers[i]) {
-                continue;
-            }
-            int temp = binarySearch(numbers, target - numbers[i], i + 1, numbers.length - 1);
- 
-            if (temp != -1) {
-                int[] out = new int[2];
-                out[0] = Math.min(temp, i) + 1;
-                out[1] = Math.max(temp, i) + 1;
-                return out;
-            }
+    int front = 0;
+    int back = numbers.length - 1;
+    while (front < back) {
+        if (numbers[front] + numbers[back] > target) {
+            back--;
         }
+        else if (numbers[front] + numbers[back] < target) {
+            front++;
+        }
+        else {
+            int[] out = {front + 1, back + 1};
+            return out;
+        }
+
+    }
+        
         
         return new int[2];
+        
     }
     
-    public int binarySearch(int[] numbers, int target, int start, int end) {
-        if (start > end) {
-            return -1;
-        }
-        int mid = (start + end)/2;
-        if (numbers[mid] == target) {
-            return mid;
-        }
-       if (numbers[mid] > target) {
-            return binarySearch(numbers, target, start, mid - 1);
-        }
-        return binarySearch(numbers, target, mid + 1, end);
-    }
+ 
 }
