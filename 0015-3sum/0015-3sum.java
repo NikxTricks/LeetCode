a95a93ab25d1a5ee234c790c992725196a235a4b
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-17 16:32:41 -0400

    Time: 30 ms (91.65%), Space: 51 MB (51.48%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,25 +6,25 @@ class Solution {
         while (target < nums.length - 2) {
             if (target > 0 && nums[target] == nums[target - 1]) {
                 target++;
-                continue;
+                continue;   
             }
             int i = target + 1;
             int j = nums.length - 1;
             while (i < j) {
                 int sum = nums[i] + nums[j] + nums[target];
                 if (sum == 0) {
-                    out.add(Arrays.asList(nums[target], nums[i], nums[j])); // Adding the triplet
-                    // Skip duplicates for i
-                    while (i < j && nums[i] == nums[i + 1]) {
-                        i++;
-                    }
-                    // Skip duplicates for j
-                    while (i < j && nums[j] == nums[j - 1]) {
-                        j--;
-                    }
-                    // Move the pointers
+                    out.add(Arrays.asList(nums[target], nums[i], nums[j]));
+                    // Increment i and decrement j after finding a triplet
                     i++;
                     j--;
+                    // Skip duplicates for i
+                    while (i < j && nums[i] == nums[i - 1]) {
+                        i++; 
+                    }
+                    // Skip duplicates for j
+                    while (i < j && nums[j] == nums[j + 1]) {
+                        j--;
+                    }
                 } else if (sum < 0) {
                     i++;
                 } else {
@@ -33,6 +33,7 @@ class Solution {
             }
             target++;
         }
+
         return out;
     }
 }
