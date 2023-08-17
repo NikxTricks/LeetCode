8a5df08657170d03ed6639222f68270265b34d42
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-17 18:11:16 -0400

    Time: 254 ms (5.00%), Space: 47.8 MB (6.57%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,62 @@
+class MinStack {
+
+    List<Integer> data;
+    PriorityQueue<Integer> min;
+    public MinStack() {
+        data = new LinkedList<>();
+        min = new PriorityQueue<>();
+    }
+    
+    public void push(int val) {
+        data.add(val);
+        min.add(val);
+    }
+    
+    public void pop() {
+        if (data.size() > 0) {
+            List<Integer> temp = new LinkedList<>();
+            int cur = data.remove(data.size() - 1);
+            System.out.println(cur);
+            for (Integer num: min) {
+                System.out.print(num);
+            }
+            int check = min.remove();
+            while (cur != check) {
+                //System.out.print(cur);
+                temp.add(check);
+                check = min.remove();
+            }
+            min.addAll(temp);
+        }
+        return;
+        
+    }
+    
+    public int top() {
+        if (data.size() > 0) {
+            return data.get(data.size() - 1);
+        }
+        
+        return -1;
+    }
+    
+    public int getMin() {
+            if (min.size() > 0) {
+                int minVal = min.peek();
+                return minVal;
+            }
+        
+        return -1;
+
+        
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
