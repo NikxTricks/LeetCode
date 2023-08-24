04dbcbf41c6574c25af687eec0d0a0dec1c2482a
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-24 14:50:09 -0400

    Time: 35 ms (72.83%), Space: 52.5 MB (6.93%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,7 +2,6 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
     Arrays.sort(nums);
-        
     List<List<Integer>> out = new LinkedList<>();
     for (int i = 0; i < nums.length; i++) {
         int j = i + 1;
@@ -10,13 +9,13 @@ class Solution {
         if (i > 0 && nums[i] == nums[i - 1]) {
             continue;
         }
-        
         while (j < k) {
             if (j > i + 1 && nums[j] == nums[j - 1]) {
                 j++;
                 continue;
             }
             if (nums[i] + nums[j] + nums[k] == 0) {
+                
                 out.add(new LinkedList(Arrays.asList(nums[i], nums[j], nums[k])));
                 j++;
             }
