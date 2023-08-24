14112ea66e3561ca1b7a8d759b19d94ac64de71e
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-24 14:50:57 -0400

    Time: 31 ms (85.36%), Space: 53 MB (5.19%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,11 +4,14 @@ class Solution {
     Arrays.sort(nums);
     List<List<Integer>> out = new LinkedList<>();
     for (int i = 0; i < nums.length; i++) {
-        int j = i + 1;
-        int k = nums.length - 1;
+        if (nums[i] > 0) {
+            break;
+        }
         if (i > 0 && nums[i] == nums[i - 1]) {
             continue;
         }
+        int j = i + 1;
+        int k = nums.length - 1;
         while (j < k) {
             if (j > i + 1 && nums[j] == nums[j - 1]) {
                 j++;
