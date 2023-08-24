7717375c0bbf472edddda411a9a5dea75ffbac45
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-08-24 14:43:44 -0400

    Time: 39 ms (62.40%), Space: 52.1 MB (9.92%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,48 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Sort the array
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        
+    Arrays.sort(nums);
+        
+    List<List<Integer>> out = new LinkedList<>();
+    int i = 0;
+    while (i < nums.length) {
+        int j = i + 1;
+        int k = nums.length - 1;
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            i++;
+            continue;
+        }
+        
+        while (j < k) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) {
+                j++;
                 continue;
             }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found a triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    // Move the pointers
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    // Sum is less than zero, increment j to increase the sum
-                    j++;
-                } else {
-                    // Sum is greater than zero, decrement k to decrease the sum
-                    k--;
-                }
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum == 0) {
+                out.add(new LinkedList(Arrays.asList(nums[i], nums[j], nums[k])));
+                j++;
+            }
+            else if (sum < 0) {
+                j++;
+            }
+            else {
+                k--;
             }
         }
-        return ans;
+        i++;
+    }
+        
+        return out;
     }
 }
