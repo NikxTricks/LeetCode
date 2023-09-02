d809dc9edaa130b401d50faeee441f3ec373850e
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-09-02 15:52:07 -0400

    Time: 59 ms (81.20%), Space: 98.8 MB (61.34%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -2,39 +2,26 @@ class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         int count = 0;
         
-        List<int[]> data = new LinkedList<int[]>();
-        for (int[] interval: intervals) {
-            data.add(interval);
-        }
-        
-        Collections.sort(data, (a, b) -> {
+        Arrays.sort(intervals, (a, b) -> {
             int out = a[1] - b[1];
             if (out == 0) {
                 out = a[0] - b[0];
             }
             return out;
-        });
+        });                
         
-        int size = data.size();
-        int i = 0;
-        while (i < size - 1) {
-        if (data.get(i)[0] == data.get(i + 1)[0] || data.get(i)[1] > data.get(i + 1)[0]) {
+        int i = 1;
+        int cur = 0;
+        
+        while (i < intervals.length) {
+        if (intervals[cur][0] == intervals[i][0] || intervals[cur][1] > intervals[i][0]) {
                 count++;
-            if (data.get(i + 1)[1] - data.get(i + 1)[0] > data.get(i)[1] - data.get(i)[0]) {
-                data.remove(i + 1);
-            }
-            else {
-                data.remove(i + 1);
-            }
-                
-                size--;
-            
-            }
-            else {
-                i++;
-            }
         }
-
+        else {
+            cur = i;
+        }
+        i++;
+        }
         
         return count;
     }
