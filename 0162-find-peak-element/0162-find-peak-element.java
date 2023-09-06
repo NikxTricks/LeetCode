604655109dc8381c9772f73f030f7fd262a54dca
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-09-06 17:20:43 -0400

    Time: 0 ms (100.00%), Space: 41.4 MB (30.92%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -9,10 +9,10 @@ class Solution {
           return start;
       }
         int mid = (start + end)/2;
-    if (nums[mid] > nums[mid + 1] && ((start == mid) || nums[mid] > nums[mid - 1])) { 
+    if (nums[mid] > nums[mid + 1] && (start == mid || nums[mid] > nums[mid - 1])) { 
 	    return mid; 
     } 
-    else if (nums[mid] < nums[mid + 1] && ((start == mid) || nums[mid] > nums[mid  - 1])) {
+    else if (nums[mid] < nums[mid + 1] && (start == mid || nums[mid] > nums[mid  - 1])) {
         return helper(nums, mid + 1, end);
     }
     else if (nums[mid] > nums[mid + 1] && nums[mid] < nums[mid - 1]) {
