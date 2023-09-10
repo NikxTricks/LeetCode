e50dbe3ebb19e11869ca83ac079d3f1bf74e85d8
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-09-09 22:47:42 -0400

    Time: 107 ms (94.47%), Space: 40.9 MB (33.14%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                if (board[i][j] == word.charAt(0)) {
-                   if (helper(board, new boolean[board.length][board[0].length], word, i, j, 0)) {
+                   if (helper(board, word, i, j, 0)) {
                        return true;
                    }
                } 
@@ -16,30 +16,30 @@ class Solution {
         
     }
     
-    public boolean helper(char[][] board, boolean[][] coords, String word, int row, int col, int count) {
-        if (coords[row][col] || board[row][col] != word.charAt(count)) {
+    public boolean helper(char[][] board, String word, int row, int col, int count) {
+        if (board[row][col] == '!' || board[row][col] != word.charAt(count)) {
             return false;
         }
         if (count == word.length() - 1 && board[row][col] == word.charAt(count)) {
             return true;
         }
-        
-        coords[row][col] = true;
+        char orig = board[row][col];
+        board[row][col] = '!';
 
-            if (row - 1 >= 0 && helper(board, coords, word, row - 1, col, count + 1)) {
+            if (row - 1 >= 0 && helper(board, word, row - 1, col, count + 1)) {
                     return true;
             }
-            if (row + 1 < board.length && helper(board, coords, word, row + 1, col, count + 1)) {
+            if (row + 1 < board.length && helper(board, word, row + 1, col, count + 1)) {
                     return true;
             }
-            if (col + 1 < board[0].length && helper(board, coords, word, row, col + 1, count + 1)) {
+            if (col + 1 < board[0].length && helper(board, word, row, col + 1, count + 1)) {
                     return true;
             }
-            if (col - 1 >= 0 && helper(board, coords, word, row, col - 1, count + 1)) {
+            if (col - 1 >= 0 && helper(board, word, row, col - 1, count + 1)) {
                     return true;
             }
     
-        coords[row][col] = false;
+        board[row][col] = orig;
         return false;
         
         
