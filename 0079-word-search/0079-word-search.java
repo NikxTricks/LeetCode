03b8d7c13767a3bb5fdb6bd6c27ddb5a40ffb50a
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-09-09 22:51:29 -0400

    Time: 115 ms (93.57%), Space: 40.8 MB (40.67%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,11 +1,11 @@
 class Solution {
-
+    String word;
     public boolean exist(char[][] board, String word) {
-        
+        this.word = word;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                if (board[i][j] == word.charAt(0)) {
-                   if (helper(board, word, i, j, 0)) {
+                   if (helper(board, i, j, 0)) {
                        return true;
                    }
                } 
@@ -16,7 +16,7 @@ class Solution {
         
     }
     
-    public boolean helper(char[][] board, String word, int row, int col, int count) {
+    public boolean helper(char[][] board, int row, int col, int count) {
         if (board[row][col] == '!' || board[row][col] != word.charAt(count)) {
             return false;
         }
@@ -26,16 +26,16 @@ class Solution {
         char orig = board[row][col];
         board[row][col] = '!';
 
-            if (row - 1 >= 0 && helper(board, word, row - 1, col, count + 1)) {
+            if (row - 1 >= 0 && helper(board, row - 1, col, count + 1)) {
                     return true;
             }
-            if (row + 1 < board.length && helper(board, word, row + 1, col, count + 1)) {
+            if (row + 1 < board.length && helper(board, row + 1, col, count + 1)) {
                     return true;
             }
-            if (col + 1 < board[0].length && helper(board, word, row, col + 1, count + 1)) {
+            if (col + 1 < board[0].length && helper(board, row, col + 1, count + 1)) {
                     return true;
             }
-            if (col - 1 >= 0 && helper(board, word, row, col - 1, count + 1)) {
+            if (col - 1 >= 0 && helper(board, row, col - 1, count + 1)) {
                     return true;
             }
     
