96d01df4e3303711fe8192e330aa33ba066569b3
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-09-10 17:25:07 -0400

    Time: 10 ms (6.63%), Space: 44.1 MB (25.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,15 +1,19 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> out = new LinkedList<>();
+        List<List<Integer>> out = new ArrayList<>();
         List<Integer> cur = new LinkedList<>();
-        helper(candidates, target, 0, out, cur, 0);
+        helper(candidates, target, 0, 0, out, cur);
         return out;
+        
     }
     
-    public void helper(int[] candidates, int target, int sum, List<List<Integer>> out, List<Integer> cur, int start) {
+    public void helper(int[] candidates, int target, int sum, int start, List<List<Integer>> out, List<Integer> cur) {
         if (sum == target) {
-            out.add(new LinkedList<>(cur));
-            return;
+            List<Integer> temp = new LinkedList<>(cur);
+            Collections.sort(temp);
+            if (!out.contains(temp)) {
+                out.add(temp);
+            }
         }
         if (sum > target) {
             return;   
@@ -17,8 +21,9 @@ class Solution {
         
         for (int i = start; i < candidates.length; i++) {
             cur.add(candidates[i]);
-            helper(candidates, target, sum + candidates[i], out, cur, i); // notice the last parameter
+            helper(candidates, target, sum + candidates[i], i, out, cur);
             cur.remove(cur.size() - 1);
         }
+        
     }
 }
