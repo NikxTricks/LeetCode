e92ecf6202140d1b320e6da95016ba569fea56ab
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-09-09 22:27:15 -0400

    Time: 111 ms (94.09%), Space: 40.6 MB (50.77%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,5 +1,5 @@
 class Solution {
-    HashSet<Integer> coords = new HashSet<>();
+    //HashSet<Integer> coords = new HashSet<>();
     public boolean exist(char[][] board, String word) {
         
         int m = board.length;
@@ -8,7 +8,7 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                if (board[i][j] == word.charAt(0)) {
-                   if (helper(board, word, i, j, 0)) {
+                   if (helper(board, new boolean[m][n], word, i, j, 0)) {
                        return true;
                    }
                } 
@@ -19,31 +19,30 @@ class Solution {
         
     }
     
-    public boolean helper(char[][] board, String word, int row, int col, int count) {
-        if (coords.contains(row * 10 + col) || board[row][col] != word.charAt(count)) {
+    public boolean helper(char[][] board, boolean[][] coords, String word, int row, int col, int count) {
+        if (coords[row][col] || board[row][col] != word.charAt(count)) {
             return false;
         }
         if (count == word.length() - 1 && board[row][col] == word.charAt(count)) {
             return true;
         }
         
-        coords.add(row * 10 + col);
+        coords[row][col] = true;
 
-            if (row - 1 >= 0 && helper(board, word, row - 1, col, count + 1)) {
+            if (row - 1 >= 0 && helper(board, coords, word, row - 1, col, count + 1)) {
                     return true;
             }
-            if (row + 1 < board.length && helper(board, word, row + 1, col, count + 1)) {
+            if (row + 1 < board.length && helper(board, coords, word, row + 1, col, count + 1)) {
                     return true;
             }
-            if (col + 1 < board[0].length && helper(board, word, row, col + 1, count + 1)) {
+            if (col + 1 < board[0].length && helper(board, coords, word, row, col + 1, count + 1)) {
                     return true;
             }
-            if (col - 1 >= 0 && helper(board, word, row, col - 1, count + 1)) {
+            if (col - 1 >= 0 && helper(board, coords, word, row, col - 1, count + 1)) {
                     return true;
             }
     
-        
-        coords.remove(row * 10 + col);
+        coords[row][col] = false;
         return false;
         
         
