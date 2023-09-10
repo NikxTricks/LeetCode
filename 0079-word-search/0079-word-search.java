c10a4802ff1f0532b6e490ba50d5084f8591c22f
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-09-09 22:28:50 -0400

    Time: 113 ms (93.77%), Space: 40.8 MB (33.14%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,14 +1,11 @@
 class Solution {
-    //HashSet<Integer> coords = new HashSet<>();
+
     public boolean exist(char[][] board, String word) {
         
-        int m = board.length;
-        int n = board[0].length;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                if (board[i][j] == word.charAt(0)) {
-                   if (helper(board, new boolean[m][n], word, i, j, 0)) {
+                   if (helper(board, new boolean[board.length][board[0].length], word, i, j, 0)) {
                        return true;
                    }
                } 
