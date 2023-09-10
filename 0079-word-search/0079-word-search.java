45e5798fe2ab99ad9279db19dd8268ccc1708b56
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-09-09 22:20:46 -0400

    Time: 395 ms (11.74%), Space: 44.4 MB (6.54%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,8 +5,6 @@ class Solution {
         int m = board.length;
         int n = board[0].length;
         
-        //char[] cur = new char[word.length()];
-        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                if (board[i][j] == word.charAt(0)) {
