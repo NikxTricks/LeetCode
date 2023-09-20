f4139e910ac5748bfa130c37e2b157cc51cd680a
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-09-20 13:27:03 -0400

    Time: 322 ms (6.16%), Space: 44.9 MB (44.44%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -8,16 +8,22 @@ class Solution {
         for (int i = 0; i < heights.length; i++) {
             for (int j = 0; j < heights[0].length; j++) {
                 if (!added[i][j]) {
-                    helper(heights, i, j, out);
+                    helper(heights, i, j);
                     visited = new boolean[heights.length][heights[0].length];
                 }
             }
         }
-        
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = 0; j < heights[0].length; j++) {
+                if (added[i][j]) {
+                    out.add(new LinkedList<>(Arrays.asList(i, j)));
+                }
+            }
+        }
         return out;
     }
     
-    public int helper(int[][] heights, int i, int j, List<List<Integer>> out) {
+    public int helper(int[][] heights, int i, int j) {
         visited[i][j] = true;
         int sum = 0;
         if (i - 1 < 0) {
@@ -25,7 +31,7 @@ class Solution {
         }
         else {
             if (heights[i - 1][j] <= heights[i][j] && !visited[i - 1][j]) {
-                sum = sum | helper(heights, i - 1, j, out);
+                sum = sum | helper(heights, i - 1, j);
             }
         }
         if (i + 1 >= heights.length) {
@@ -33,7 +39,7 @@ class Solution {
         }
         else {
             if (heights[i + 1][j] <= heights[i][j] && !visited[i + 1][j]) {
-                sum = sum | helper(heights, i + 1, j, out);
+                sum = sum | helper(heights, i + 1, j);
             }
         }
         if (j - 1 < 0) {
@@ -41,7 +47,7 @@ class Solution {
         }
         else {
             if (heights[i][j - 1] <= heights[i][j] && !visited[i][j - 1]) {
-                sum = sum | helper(heights, i, j - 1, out);
+                sum = sum | helper(heights, i, j - 1);
             }
         }
         if (j + 1 >= heights[0].length) {
@@ -49,11 +55,11 @@ class Solution {
         }
         else {
             if (heights[i][j + 1] <= heights[i][j] && !visited[i][j + 1]) {
-                sum = sum | helper(heights, i, j + 1, out);
+                sum = sum | helper(heights, i, j + 1);
             }
         }
         if (sum == 3 && !added[i][j]) {
-            out.add(new LinkedList<>(Arrays.asList(i, j)));
+            //out.add(new LinkedList<>(Arrays.asList(i, j)));
             added[i][j] = true;
         }
         
