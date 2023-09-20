531f64ea2f790789f0c2e73689ac59ec25a1bac9
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-09-19 20:51:17 -0400

    Time: 2 ms (100.00%), Space: 47.6 MB (54.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,14 +2,13 @@ class Solution {
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
         int count = 0;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
+                if (grid[i][j] == '1') {
                     count++;
-                    helper(grid, i, j, visited);
+                    helper(grid, i, j);
                 }
             }
         }
@@ -18,22 +17,22 @@ class Solution {
     }
     
     
-    public void helper(char[][] grid, int i, int j, boolean[][] visited) {
-        if (grid[i][j] == '0' || visited[i][j] == true) {
+    public void helper(char[][] grid, int i, int j) {
+        if (grid[i][j] == '0' || grid[i][j] == '2') {
             return;
         }
-        visited[i][j] = true;
+        grid[i][j] = '2';
         if (i + 1 < grid.length) {
-            helper(grid, i + 1, j, visited);
+            helper(grid, i + 1, j);
         }
         if (i - 1 >= 0) {
-            helper(grid, i - 1, j, visited);
+            helper(grid, i - 1, j);
         }
         if (j + 1 < grid[0].length) {
-            helper(grid, i, j + 1, visited);
+            helper(grid, i, j + 1);
         }
         if (j - 1 >= 0) {
-            helper(grid, i, j - 1, visited);
+            helper(grid, i, j - 1);
         }
         
     }
