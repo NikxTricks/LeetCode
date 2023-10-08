e84b1a755faa9e4b7dc83953c02f6104eba03585
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-10-08 14:45:05 -0400

    Time: 0 ms (100.00%), Space: 39.6 MB (74.08%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] data1 = new int[nums.length - 1];
+        int[] data2 = new int[nums.length];
+        int[] nums1 = Arrays.copyOfRange(nums, 1, nums.length);
+        Arrays.fill(data1, -1);
+        Arrays.fill(data2, -1);
+        
+            return Math.max(helper(nums1.length - 1, nums1, data1), helper(nums.length - 2, nums, data2));
+
+    }
+    
+    public int helper(int n, int[] nums, int[] data) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return nums[0];
+        }
+        if (data[n] != -1) {
+            return data[n];
+        }
+        else {
+            data[n] = Math.max(helper(n - 2, nums, data) + nums[n], helper(n - 1, nums, data));
+            return data[n];
+        }
+    }
+}
