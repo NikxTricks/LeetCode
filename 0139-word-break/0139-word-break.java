a75d04aacc3e7f26de749bbc84927e2704826073
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-10-14 18:48:16 -0400

    Time: 12 ms (9.74%), Space: 43.6 MB (31.87%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashSet<String> dict = new HashSet<>(wordDict);
+        //HashSet<String> dict = new HashSet<>(wordDict);
         
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
         
         for (int i = 1; i < s.length() + 1; i++) {
             for (int j = 0; j < i; j++) {
-                if (dict.contains(s.substring(j, i)) && dp[j] == true) {
+                if (wordDict.contains(s.substring(j, i)) && dp[j] == true) {
                     dp[i] = true;
                     break;
                 }
