330ae7ec46f0afd010bc7e4b9a79052568a4b9a5
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-10-15 13:05:22 -0400

    Time: 44 ms (44.88%), Space: 43 MB (71.67%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,17 +3,18 @@ class Solution {
         //optimize with binary search
         int[] dp = new int[nums.length];
         int max = 0;
-        int start = 0;
-        while (start < nums.length - 1 && nums[start] > nums[start + 1]) {
-            start++;
+        int i = 0;
+        while (i < nums.length - 1 && nums[i] > nums[i + 1]) {
+            i++;
         }
-        for (int i = start; i < nums.length; i++) {
+        while (i < nums.length) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                     max = Math.max(dp[i], max);
                 }
             }
+            i++;
         }
         
         return ++max;
