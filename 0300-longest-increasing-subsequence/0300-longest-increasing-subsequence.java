5a33afed581f051fecf71080a3f402c8728eaafc
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-10-15 12:57:13 -0400

    Time: 43 ms (47.61%), Space: 42.3 MB (99.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,7 +3,11 @@ class Solution {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
         int max = 1;
-        for (int i = 1; i < nums.length; i++) {
+        int start = 0;
+        while (start < nums.length - 1 && nums[start] > nums[start + 1]) {
+            start++;
+        }
+        for (int i = start; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
