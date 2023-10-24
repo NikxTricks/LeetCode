d4e014d299b60a586e79f2fc7a731fd3e94ff1fd
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-10-24 13:14:06 -0400

    Time: 463 ms (5.02%), Space: 45.3 MB (6.25%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -9,12 +9,20 @@ class Solution {
         }
         for (int i = 1; i <= word1.length(); i++) {
             for (int j = 1; j <= word2.length(); j++) {
-if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-    dp[i][j] = dp[i-1][j-1];
-} else {
-    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j]));
-}
+            if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                dp[i][j] = dp[i-1][j-1];
+            } 
+            else {
+                dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j]));
+                }
             }
+        }
+        
+        for (int[] row: dp) {
+            for (int i: row) {
+                System.out.print(i + " ");
+            }
+            System.out.println("");
         }
         
         return dp[word1.length()][word2.length()];
