35eaa6291cb375b3266b6fc5cb0e301f84db7c30
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2023-10-23 23:38:34 -0400

    Time: 42 ms (14.08%), Space: 44.6 MB (9.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -7,7 +7,7 @@ class Solution {
         for (int i = 1; i <= amount; i++) {
             for (int j = 1; j <= coins.length; j++) {
                 if (coins[j - 1] > i) {
-                    dp[i][j] = Math.max(dp[i][j - 1], -1);
+                    dp[i][j] = dp[i][j - 1];
                 }
                 else {
                     if (dp[i][j - 1] != -1 && dp[i - coins[j - 1]][j] != -1) {
