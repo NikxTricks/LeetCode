caa684f8e4a6d761ea8c2839e8a94f0089100571
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-03-07 15:37:56 -0500

    Time: 1488 ms (5.03%), Space: 45.3 MB (83.12%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,12 +4,10 @@ class Solution {
         dp[0] = true;
         for (int i = 0; i < nums.length; i++) {
             if (dp[i]) {
-                //System.out.println(nums[i]);
                 for (int j = 1; j <= nums[i]; j++) {
                     if (i + j < nums.length) {
                         dp[i + j] = true;
                     }
-                   // System.out.println(i + j);
                 } 
             }
         }   
