9f2c7ad9051530b733b2e167cc241c75fb3b7cd4
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-03-07 15:43:26 -0500

    Time: 91 ms (16.59%), Space: 45.3 MB (91.21%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean canJump(int[] nums) {
         boolean[] dp = new boolean[nums.length];
         dp[0] = true;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             if (dp[i]) {
                 for (int j = 1; j <= nums[i]; j++) {
                     if (nums[i] >= nums.length - 1 - i) {
