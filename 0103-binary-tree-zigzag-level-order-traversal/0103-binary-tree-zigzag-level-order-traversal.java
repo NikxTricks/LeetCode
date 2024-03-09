939630361506957d3dd622cdd9e594788cea0a78
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-03-09 15:58:29 -0500

    Time: 1 ms (76.65%), Space: 42.2 MB (26.03%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,42 +15,44 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        Stack<TreeNode> s = new Stack<>();
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
         List<List<Integer>> out = new LinkedList<>();
         if (root == null) {
             return out;
         }
 
-        q.add(root);
-        boolean zig = true;
-        while (q.size() != 0) {
-            int levelSize = q.size();
+        s1.add(root);
+        
+        while (!s1.isEmpty() || !s2.isEmpty()) {
             List<Integer> subOut = new LinkedList<>();
-            for (int i = 0; i < levelSize; i++) {
-                if (q.peek().left != null) {
-                    q.add(q.peek().left);
-                    if (zig) {
-                       s.add(q.peek().left); 
-                    }
+            while (!s1.isEmpty()) {
+                if (s1.peek().left != null) {
+                    s2.add(s1.peek().left);
                 }
-                if (q.peek().right != null) {
-                    q.add(q.peek().right);
-                    if (zig) {
-                        s.add(q.peek().right);
-                    }
-                }  
-                if (zig) {
-                    subOut.add(q.remove().val);
+                if (s1.peek().right != null) {
+                    s2.add(s1.peek().right);
                 }
-                else {
-                    subOut.add(s.pop().val);
-                    q.remove();
-                }
+                subOut.add(s1.pop().val);
             }
-            out.add(subOut);
-            zig = !zig;
+            if (!subOut.isEmpty()) {
+                out.add(subOut);
+            }
+            List<Integer> subOutN = new LinkedList<>();
+            while (!s2.isEmpty()) {
+                if (s2.peek().right != null) {
+                    s1.add(s2.peek().right);
+                }
+                if (s2.peek().left != null) {
+                    s1.add(s2.peek().left);
+                }
+                subOutN.add(s2.pop().val);
+            }
+            if (!subOutN.isEmpty()) {
+                out.add(subOutN);
+            }
         }
+
         return out;
     }
 }
