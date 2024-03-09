4797f67f0fe32ad4f303339fe39ebd0f10c1c3f5
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-03-09 14:40:46 -0500

    Time: 2 ms (17.24%), Space: 44.9 MB (57.17%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,26 +13,53 @@
  *     }
  * }
  */
+//next is BFS appraoch, using a Queue to line up nodes
+//DFS approach
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        int count = 0;
-        List<List<Integer>> out = new LinkedList<>();
-        helper(root, out, count);
-        return out;
         
+        Queue<TreeNode> f = new LinkedList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> out = new LinkedList<>();
+        
+        if (root == null) {
+            return out;
+        }
+        
+        f.add(root);
+        
+        TreeNode cur = null;
+        while (f.size() != 0 || q.size() != 0) {
+            if (f.size() != 0) {
+               out.add(new LinkedList<Integer>()); 
+            }
+            while (f.size() != 0) {
+                cur = f.remove();
+                
+                out.get(out.size() - 1).add(cur.val);
+                if (cur.left != null) {
+                    q.add(cur.left);
+                }
+                if (cur.right != null) {
+                    q.add(cur.right);
+                }
+            }
+            if (q.size() != 0) {
+               out.add(new LinkedList<Integer>()); 
+            }
+            while (q.size() != 0) {
+                cur = q.remove();
+                out.get(out.size() - 1).add(cur.val);
+                if (cur.left != null) {
+                    f.add(cur.left);
+                }
+                if (cur.right != null) {
+                    f.add(cur.right);
+                } 
+            }
+        }
+        
+        return out;
     }
     
-    public void helper(TreeNode cur, List<List<Integer>> out, int count) {
-        if (cur == null) {
-            return;
-        }
-        if (count == out.size()) {
-            out.add(new LinkedList<Integer>(Arrays.asList(cur.val)));
-        }
-        else {
-            out.get(count).add(cur.val);
-        }
-        helper(cur.left, out, ++count);
-        helper(cur.right, out, count);
-    }
 }
