7bee09fb14b59ceb27388ca98a47bbe79ffc136f
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-03-09 00:14:14 -0500

    Time: 3 ms (97.83%), Space: 44.5 MB (51.08%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,44 +1,15 @@
 class Solution {
     public String intToRoman(int num) {
         StringBuilder numeral = new StringBuilder();
-        HashMap<Integer, String> numerals = new HashMap<>();
-        numerals.put(1000, "M");
-        numerals.put(900, "CM");
-        numerals.put(500, "D");
-        numerals.put(400, "CD");
-        numerals.put(100, "C");
-        numerals.put(90, "XC");
-        numerals.put(50, "L");
-        numerals.put(40, "XL");
-        numerals.put(10, "X");
-        numerals.put(9, "IX");
-        numerals.put(5, "V");
-        numerals.put(4, "IV");
-        numerals.put(1, "I");
-        
-        List<Integer> nums = new ArrayList<>();
-        nums.add(1000);
-        nums.add(900);
-        nums.add(500);
-        nums.add(400);
-        nums.add(100);
-        nums.add(90);
-        nums.add(50);
-        nums.add(40);
-        nums.add(10);
-        nums.add(9);
-        nums.add(5);
-        nums.add(4);
-        nums.add(1);
-        
+        int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int i = 0;
-        int cur = 1000;
         while (num > 0) {
-            while (cur > num) {
-                cur = nums.get(++i);
+            while (numbers[i] > num) {
+                i++;
             }
-            numeral.append(numerals.get(cur));
-            num -= cur;
+            numeral.append(numerals[i]);
+            num -= numbers[i];
             
         }
         
