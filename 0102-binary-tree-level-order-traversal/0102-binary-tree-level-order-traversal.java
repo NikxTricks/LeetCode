cc667550597db38f94bdca0035b9fbec5dccb69c
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-03-09 15:06:14 -0500

    Time: 1 ms (88.37%), Space: 45.2 MB (14.14%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,52 +13,34 @@
  *     }
  * }
  */
-//next is BFS appraoch, using a Queue to line up nodes
-//DFS approach
+
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         
         Queue<TreeNode> f = new LinkedList<>();
-        Queue<TreeNode> q = new LinkedList<>();
         List<List<Integer>> out = new LinkedList<>();
-        
+
         if (root == null) {
             return out;
         }
-        
         f.add(root);
         
         TreeNode cur = null;
-        while (f.size() != 0 || q.size() != 0) {
-            if (f.size() != 0) {
-               out.add(new LinkedList<Integer>()); 
-            }
-            while (f.size() != 0) {
+        while (f.size() != 0) {
+            int levelSize = f.size();
+            List<Integer> subOut = new LinkedList<>();
+            for (int i = 0; i < levelSize; i++) {
                 cur = f.remove();
-                
-                out.get(out.size() - 1).add(cur.val);
-                if (cur.left != null) {
-                    q.add(cur.left);
-                }
-                if (cur.right != null) {
-                    q.add(cur.right);
-                }
-            }
-            if (q.size() != 0) {
-               out.add(new LinkedList<Integer>()); 
-            }
-            while (q.size() != 0) {
-                cur = q.remove();
-                out.get(out.size() - 1).add(cur.val);
+                subOut.add(cur.val);
                 if (cur.left != null) {
                     f.add(cur.left);
                 }
                 if (cur.right != null) {
                     f.add(cur.right);
-                } 
+                }
             }
+            out.add(subOut);
         }
-        
         return out;
     }
     
