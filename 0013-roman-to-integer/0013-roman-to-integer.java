0530f887e6f11f5d06745d20d6723f538f245d3b
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-03-11 01:34:08 -0400

    Time: 8 ms (9.22%), Space: 44.5 MB (71.69%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -9,8 +9,9 @@ class Solution {
         numbers.put('C', 100);
         numbers.put('D', 500);
         numbers.put('M', 1000);
+        int f = 0;
         for (int i = 0; i < s.length(); i++) {
-            int f = numbers.get(s.charAt(i));
+            f = numbers.get(s.charAt(i));
             if (i < s.length() - 1 && numbers.get(s.charAt(i + 1)) > f) {
                 decimal += numbers.get(s.charAt(i + 1)) - f;
                 i++;
