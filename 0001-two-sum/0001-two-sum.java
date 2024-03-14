720ddc30277ec698b79693e0ac7747fc9f348a60
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-03-13 23:36:43 -0400

    Time: 5 ms (59.04%), Space: 44.7 MB (75.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,41 @@
 import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> data = new HashMap<>();
-        
+        int[] temp = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            if (data.containsKey(target - nums[i])) {
-                int[] out = {data.get(target - nums[i]), i};
-                return out;
+            temp[i] = nums[i];
+        }
+        int[] values = new int[2];
+        int[] indicies = {-1, -1};
+        Arrays.sort(nums);
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] == target) {
+               values[0] = nums[i]; 
+                values[1] = nums[j];
             }
-            data.put(nums[i], i);
-        }   
-        return new int[1];
+            if (nums[i] + nums[j] < target) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        
+        for (int k = 0; k < nums.length; k++) {
+            if (indicies[0] == -1 && values[0] == temp[k]) {
+                indicies[0] = k;
+            }
+            else if (indicies[1] == -1 && values[1] == temp[k]) {
+                indicies[1] = k;
+            }
+        }
+        
+        return indicies;
+        
+        
+        
     }
 }
 
