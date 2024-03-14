cdba143433240c4744536878b45414a936e2e643
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-03-13 23:54:25 -0400

    Time: 35 ms (45.39%), Space: 53.7 MB (5.74%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
-    Arrays.sort(nums);
-    List<List<Integer>> out = new LinkedList<>();
-    for (int i = 0; i < nums.length; i++) {
-        if (nums[i] > 0) {
-            break;
-        }
-        if (i > 0 && nums[i] == nums[i - 1]) {
-            continue;
-        }
-        int j = i + 1;
-        int k = nums.length - 1;
-        while (j < k) {
-            if (j > i + 1 && nums[j] == nums[j - 1]) {
-                j++;
+        Arrays.sort(nums);
+        List<List<Integer>> out = new LinkedList<>();
+        int i = 0;
+        while (i < nums.length) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                i++;
                 continue;
             }
-            if (nums[i] + nums[j] + nums[k] == 0) {
-                
-                out.add(new LinkedList(Arrays.asList(nums[i], nums[j], nums[k])));
-                j++;
+            int k = i + 1;
+            int j = nums.length - 1;
+            while (k < j) {
+            if (k > i + 1 && nums[k] == nums[k - 1]) {
+                k++;
+                continue;
             }
-            else if (nums[i] + nums[j] + nums[k] < 0) {
-                j++;
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                   out.add(new LinkedList<>(Arrays.asList(nums[i], nums[j], nums[k])));
+                    k++;
+               }  
+                else if (nums[i] + nums[j] + nums[k] < 0) {
+                   k++;
+               }
+                else {
+                    j--;
+                }
             }
-            else {
-                k--;
-            }
+            i++;
         }
-    }
+        
+        
         
         return out;
     }
