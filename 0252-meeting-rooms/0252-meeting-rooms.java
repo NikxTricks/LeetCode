870f918b8f34379ebb398b822b3d20df98ca27df
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-05-28 00:41:17 -0400

    Time: 6 ms (78.54%), Space: 45.9 MB (42.59%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,19 +1,12 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        PriorityQueue<int[]> order = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        for (int[] interval: intervals) {
-            order.add(interval);
-        }
-    
-        int prev = 0;
-       while (order.size() > 0) {
-           int[] cur = order.remove();
-           if (prev > cur[0]) {
-               return false;
-           }
-           prev = cur[1];
-       }
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
         
         return true;
     
