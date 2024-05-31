041f9e934d9dbd81fd2815485170e39ddda5a91f
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-05-31 19:11:08 -0400

    Time: 1 ms (98.56%), Space: 44.9 MB (34.49%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,36 +1,19 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> out = new LinkedList<>();
-        if (intervals.length == 0) {
-            int[][] done = {{newInterval[0], newInterval[1]}};
-            return done;
-        }
-        int[] add = new int[2];
         int i = 0;
         while (i < intervals.length && newInterval[0] > intervals[i][1] ) {
             out.add(intervals[i]);
             i++;
         }
-        if (i >= intervals.length) {
-            add[0] = newInterval[0];
-        }
-        else {
-          add[0] = Math.min(newInterval[0], intervals[i][0]);  
-        }
-        while (i < intervals.length && newInterval[1] > intervals[i][1]) {
+        if (i < intervals.length) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        } 
+        while (i < intervals.length && newInterval[1] >= intervals[i][0]) {
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
         }
-        if (i >= intervals.length) {
-            i--;
-        }
-        if (newInterval[1] < intervals[i][0]) {
-            add[1] = newInterval[1];
-        }
-        else {
-            add[1] = Math.max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-        out.add(add);
+        out.add(newInterval);
         for (int j = i; j < intervals.length; j++) {
             out.add(intervals[j]);
         }
