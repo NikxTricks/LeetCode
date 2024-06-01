c0045994584c0e07dc60c0b92c2b15f40eeac367
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-05-31 20:07:47 -0400

    Time: 8 ms (86.85%), Space: 46.6 MB (21.58%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -2,20 +2,22 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         //implement using in-place array (copy of array)
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        //List<int[]> out = new ArrayList<>();
+        List<int[]> out = new ArrayList<>();
         int cur = 0;
         int j = 1;
-        int index = 0;
+        //int index = 0;
         while (cur < intervals.length) {
             while (j < intervals.length && intervals[cur][1] >= intervals[j][0]) {
                 intervals[cur][1] = Math.max(intervals[cur][1], intervals[j][1]);
                 j++;
             }
-            intervals[index] = intervals[cur];
+            //intervals[index] = intervals[cur];
+            out.add(intervals[cur]);
             cur = j;
-            index++;
+            //index++;
         }
-        return Arrays.copyOf(intervals, index);
+        //return Arrays.copyOf(intervals, index);
+        return out.toArray(new int[out.size()][]);
     }
     
 }
