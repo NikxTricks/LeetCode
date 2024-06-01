4c619bf449b054906ddfbd3d1478098b71a73dd0
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-06-01 11:19:38 -0400

    Time: 8 ms (65.43%), Space: 45.9 MB (46.18%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -2,12 +2,11 @@ import java.util.Set;
 import java.util.LinkedList;
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        int max = 0;
+        int max = 1;
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         PriorityQueue<Integer> order = new PriorityQueue<>();
-        order.add(-1);
         for (int[] meetingTime: intervals) {
-            if (order.size() == 0 || meetingTime[0] >= order.peek()) {
+            if (order.size() > 0 && meetingTime[0] >= order.peek()) {
                 order.remove();
             }
             order.add(meetingTime[1]);
