419a58c03dbdac4d47b7c6bdd3cf2f4fca3295c7
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-06-02 18:46:32 -0400

    Time: 1 ms (37.11%), Space: 44.5 MB (28.62%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -13,25 +13,31 @@
  *     }
  * }
  */
-class Solution {
-    public int k; //number traversed
-    public int min; //return
-    
+class Solution { 
     public int kthSmallest(TreeNode root, int k) {
-        helper(root, k);
-        return this.min;
-    }
-    
-    public void helper(TreeNode cur, int k) {
-        if (cur == null) {
-            return;
+        int min = root.val;
+        int count = 0;
+        Stack<TreeNode> order = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null) {
+            order.add(cur);
+            cur = cur.left;
         }
-        helper(cur.left, k);
-        this.k++;
-        if (this.k == k) {
-                this.min = cur.val;
+        while (order.size() != 0) {
+            cur = order.pop();
+            if (++count == k) {
+                min = cur.val;
+                return min;
+            }
+            cur = cur.right;
+            while (cur != null) {
+                order.add(cur);
+                cur = cur.left;
+            }
         }
-        helper(cur.right, k);
+        return min;
+        
     }
+
 
 }
