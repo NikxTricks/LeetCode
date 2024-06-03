52aae1a02586e2708361d3c872f7dbce4d56d530
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-06-03 19:17:55 -0400

    Time: 13 ms (5.33%), Space: 44.7 MB (38.56%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.java
+++ 0261-graph-valid-tree/0261-graph-valid-tree.java
@@ -6,19 +6,18 @@ class Solution {
         if (edges.length == 0) {
             return true;
         }
-        //HashMap<Integer, Integer> sets = new HashMap<>();
         int[] sets = new int[n];
         Arrays.fill(sets, -1);
         for (int[] edge: edges) {
+            System.out.println(edge[0] + ", " + edge[1]);
             if (sets[edge[0]] != -1 && sets[edge[1]] != -1 && sets[edge[0]] == sets[edge[1]]) {
-                //System.out.println(edge[0]"Here");
                 return false;
             }
             else if (sets[edge[0]] != -1) {
-                sets[edge[1]] = sets[edge[0]];
+                merge(edge[1], sets, sets[edge[0]]);
             }
             else if (sets[edge[1]] != -1) {
-                sets[edge[0]] = sets[edge[1]];
+                merge(edge[0], sets, sets[edge[1]]);
             }
             else {
                 sets[edge[0]] = edge[0];
@@ -26,10 +25,25 @@ class Solution {
             }
         }
         
-
-        
         return true;
         
     }
+    
+    public void merge(int vertex, int[] sets, int parent) {
+        if (sets[vertex] == -1) {
+            sets[vertex] = parent;
+           return; 
+        }
+        if (sets[vertex] != vertex) {
+            merge(sets[vertex], sets, parent);
+        }
+        sets[vertex] = parent;
+
+    }
+    
+    /*
+    7
+[[0,1],[2,3],[1,3],[0,2],[4,5],[5,6]]
+    */
     
 }
