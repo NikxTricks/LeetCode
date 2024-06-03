162aacf6aefc28df7463f2a6d682c6a522103cd1
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-06-03 18:07:19 -0400

    Time: 0 ms (100.00%), Space: 44.6 MB (54.91%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.java
+++ 0261-graph-valid-tree/0261-graph-valid-tree.java
@@ -6,26 +6,27 @@ class Solution {
         if (edges.length == 0) {
             return true;
         }
-        HashMap<Integer, Integer> sets = new HashMap<>();
+        //HashMap<Integer, Integer> sets = new HashMap<>();
+        int[] sets = new int[n];
+        Arrays.fill(sets, -1);
         for (int[] edge: edges) {
-            if (sets.containsKey(edge[0]) && sets.containsKey(edge[1]) && sets.get(edge[0]) == sets.get(edge[1])) {
+            if (sets[edge[0]] != -1 && sets[edge[1]] != -1 && sets[edge[0]] == sets[edge[1]]) {
+                //System.out.println(edge[0]"Here");
                 return false;
             }
-            else if (sets.containsKey(edge[0])) {
-                sets.put(edge[1], sets.get(edge[0]));
+            else if (sets[edge[0]] != -1) {
+                sets[edge[1]] = sets[edge[0]];
             }
-            else if (sets.containsKey(edge[1])) {
-                sets.put(edge[0], sets.get(edge[1]));
+            else if (sets[edge[1]] != -1) {
+                sets[edge[0]] = sets[edge[1]];
             }
             else {
-                sets.put(edge[0], edge[0]);
-                sets.put(edge[1], edge[0]);
+                sets[edge[0]] = edge[0];
+                sets[edge[1]] = edge[0];
             }
         }
         
-        if (sets.size() != n) {
-            return false;
-        }
+
         
         return true;
         
