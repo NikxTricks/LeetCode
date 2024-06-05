e578c862e2a1b5c91a0293cdcf03af6109ecda11
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-06-05 15:55:42 -0400

    Time: 17 ms (10.25%), Space: 44.2 MB (86.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,15 +2,12 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] data = new int[numCourses];
         HashMap<Integer, Integer> in = new HashMap<>();
-        for (int i = 0; i < numCourses; i++) {
-            in.put(i, 0);
-        }
         for (int[] prereq: prerequisites) {
-            in.put(prereq[1], in.get(prereq[1]) + 1);
+            in.put(prereq[1], in.getOrDefault(prereq[1], 0) + 1);
         }
         Queue<Integer> order = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
-            if (in.get(i) == 0) {
+            if (in.getOrDefault(i, 0) == 0) {
                 order.add(i);
             }
         }
