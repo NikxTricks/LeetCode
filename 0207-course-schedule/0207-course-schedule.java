35d3829c30ef73455dd7d9c532cb52688b7483c9
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-06-05 16:33:19 -0400

    Time: 13 ms (12.96%), Space: 44.4 MB (72.94%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,8 +1,13 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] data = new int[numCourses];
+        List<List<Integer>> adj = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new LinkedList<>());
+        }
         for (int[] prereq: prerequisites) {
             data[prereq[1]]++;
+            adj.get(prereq[0]).add(prereq[1]);
         }
         Queue<Integer> order = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
@@ -12,23 +17,20 @@ class Solution {
         }
         while (order.size() > 0) {
                 int cur = order.remove();
-                    for (int[] prereq: prerequisites) {
-                        if (prereq[0] == cur) {
-                            data[prereq[1]]--;
-                            if (data[prereq[1]] == 0) {
-                               order.add(prereq[1]); 
+                    for (int neighbor: adj.get(cur)) {
+                            data[neighbor]--;
+                            if (data[neighbor] == 0) {
+                               order.add(neighbor); 
                             }
                         }
-                    }  
+                    
                     numCourses--;
-            }
+        }
         if (numCourses != 0) {
             return false;
         }
-        
         return true;
-
-
+    
     }
     
 }
