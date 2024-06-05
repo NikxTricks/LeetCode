15e638c755d64399385dfbbfc216ff743854cca0
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-06-05 15:54:06 -0400

    Time: 17 ms (10.25%), Space: 44.8 MB (50.08%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -8,22 +8,25 @@ class Solution {
         for (int[] prereq: prerequisites) {
             in.put(prereq[1], in.get(prereq[1]) + 1);
         }
-        while (in.size() > 0 && in.containsValue(0)) {
-            for (int i = 0; i < numCourses; i++) {
-                int curVertexInDegree = in.getOrDefault(i, -1);
-                if (curVertexInDegree == 0) {
-                    System.out.println(i);
+        Queue<Integer> order = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (in.get(i) == 0) {
+                order.add(i);
+            }
+        }
+        while (order.size() > 0) {
+                int cur = order.remove();
                     for (int[] prereq: prerequisites) {
-                        if (prereq[0] == i) {
-                            in.put(prereq[1], in.get(prereq[1]) - 1);
+                        if (prereq[0] == cur) {
+                            int deg = in.get(prereq[1]) - 1;
+                            in.put(prereq[1], deg);
+                            if (deg == 0) {
+                               order.add(prereq[1]); 
+                            }
                         }
                     }  
-                    in.remove(i);
-                }
+                    in.remove(cur);
             }
-            
-        }
-        
         if (in.size() > 0) {
                 return false;  
         }
