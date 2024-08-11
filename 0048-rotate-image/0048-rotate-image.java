c895fd29dbfeafda88784f6f0df20aecbedb3de3
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-08-10 21:33:21 -0700

    Time: 0 ms (100.00%), Space: 42.3 MB (13.99%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -11,21 +11,6 @@ class Solution {
                 matrix[n - 1 - (i - start)][start] = matrix[n - 1][n - 1 - (i - start)];
                 matrix[n - 1][n - 1 - (i - start)] = matrix[i][n - 1];
                 matrix[i][n - 1] = temp;
-                
-                
-                
-                /*
-            temp = matrix[i][n - 1];
-            matrix[i][n - 1] = matrix[start][i];
-            
-            tempNext = matrix[n - 1][n - 1 - (i - start)];
-            matrix[n - 1][n - 1 - (i - start)] = temp;
-            
-            temp = matrix[n - 1 - (i - start)][start];
-            matrix[n - 1 - (i - start)][start] = tempNext;
-            
-            matrix[start][i] = temp;
-            */
         }
         
         start++;
