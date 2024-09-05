ff11f068536fc162db4e0ed54e3a8b826aa10fda
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-09-04 17:43:06 -0700

    Time: 115 ms (19.82%), Space: 45.9 MB (7.97%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -12,8 +12,6 @@ class Solution {
         
         int[] sCount = new int[58];
         int[] tCount = new int[58];
-        //Map<Character, Integer> sCount = new HashMap<>();
-        //Map<Character, Integer> tCount = new HashMap<>();
         
         for (int w = 0; w < tArr.length; w++) {
             tCount[tArr[w] - 'A']++;
@@ -21,9 +19,7 @@ class Solution {
         
         while (j < s.length()) {
             while (j < s.length() && !check(sCount, tCount)) {
-                if (tCount[sArr[j] - 'A'] != 0) {
-                    sCount[sArr[j] - 'A']++;
-                }
+                sCount[sArr[j] - 'A']++;
                 j++;
             }
             while (i < s.length() && check(sCount, tCount)) {
@@ -31,9 +27,7 @@ class Solution {
                     out = s.substring(i, j);
                     min = j - i - 1;
                 }
-                if (sCount[sArr[i] - 'A'] != 0) {
-                    sCount[sArr[i] - 'A']--;
-                }
+                sCount[sArr[i] - 'A']--;
                 i++; 
             }
         }
@@ -42,7 +36,7 @@ class Solution {
     
     public boolean check(int[] sCount, int[] tCount) {
         for (int i = 0; i < sCount.length; i++) {
-            if (sCount[i] < tCount[i]) {
+            if (tCount[i] != 0 && sCount[i] < tCount[i]) {
                 return false;
             }
         }
