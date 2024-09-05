5d1ad340eadafc73d700b761625afe613272eb22
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-09-04 18:50:29 -0700

    Time: 2 ms (99.87%), Space: 44.3 MB (94.37%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -5,6 +5,9 @@ class Solution {
         int minI = 0;
         int minJ = 0;
         
+        int check = 0;
+        int formed = 0;
+        
         String out = "";
         
         int i = 0;
@@ -17,34 +20,43 @@ class Solution {
         int[] tCount = new int[58];
         
         for (int w = 0; w < tArr.length; w++) {
+            if (tCount[tArr[w] - 'A'] == 0) {
+                check++;
+            }
             tCount[tArr[w] - 'A']++;
         }
         
         while (j < s.length()) {
-            while (j < s.length() && !check(sCount, tCount)) {
+            while (j < s.length() && formed < check) {
                 sCount[sArr[j] - 'A']++;
+                if (sCount[sArr[j] - 'A'] == tCount[sArr[j] - 'A'] && tCount[sArr[j] - 'A'] != 0) {
+                    formed++;
+                }
                 j++;
             }
-            while (i < s.length() && check(sCount, tCount)) {
+            while (i < s.length() && formed >= check) {
                 if (j - i - 1 < min) {
                     min = j - i - 1;
                     minI = i;
                     minJ = j;
                 }
                 sCount[sArr[i] - 'A']--;
+                if (tCount[sArr[i] - 'A'] != 0 && sCount[sArr[i] - 'A'] < tCount[sArr[i] - 'A']) {
+                    formed--;
+                }
                 i++; 
             }
         }
         return s.substring(minI, minJ);
     }
     
-    public boolean check(int[] sCount, int[] tCount) {
-        for (int i = 0; i < sCount.length; i++) {
-            if (tCount[i] != 0 && sCount[i] < tCount[i]) {
-                return false;
-            }
-        }
+//     public boolean check(int[] sCount, int[] tCount) {
+//         for (int i = 0; i < sCount.length; i++) {
+//             if (tCount[i] != 0 && sCount[i] < tCount[i]) {
+//                 return false;
+//             }
+//         }
         
-        return true;
-    }
+//         return true;
+//     }
 }
