e0b8441fed3076063f7370ba7e005759f2447c4a
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-09-16 23:11:16 -0400

    Time: 69 ms (59.62%), Space: 57.3 MB (60.59%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -6,31 +6,34 @@ class Solution {
         int max = 0;
         Stack<Integer> inc = new Stack<>();
         int i = 0;
-        while (i < hist.length) {
-            if (inc.size() == 0 || hist[inc.peek()] <= hist[i]) {
+        while (i <= hist.length) {
+            int h = (i == hist.length) ? 0 : hist[i];
+            if (inc.size() == 0 || hist[inc.peek()] <= h) {
                 inc.push(i);
                 i++;
             }
             else {
                 int cur = inc.pop();
                 if (inc.isEmpty()) {
-                   max = Math.max(hist[cur] * (i), max);
+                    //System.out.printf("Deafult; %d * %d\n", hist[cur], i);
+                   max = Math.max(hist[cur] * i, max);
                 }
                 else {
                     max = Math.max(hist[cur] * (i - inc.peek() - 1), max);
+                    //System.out.printf("Main: %d * %d\n", hist[cur], i - inc.peek() - 1);
                 }
             }
         }
         
-        while (!inc.isEmpty()) {
-                int cur = inc.pop();
-                if (inc.isEmpty()) {
-                   max = Math.max(hist[cur] * (i), max);
-                }
-                else {
-                    max = Math.max(hist[cur] * (i - inc.peek() - 1), max);
-                }
-        }
+        // while (!inc.isEmpty()) {
+        //         int cur = inc.pop();
+        //         if (inc.isEmpty()) {
+        //            max = Math.max(hist[cur] * Math.min(i, hist[cur]), max);
+        //         }
+        //         else {
+        //             max = Math.max(hist[cur] * Math.min((i - inc.peek() - 1), hist[cur]), max);
+        //         }
+        // }
         
         return max;
     }
