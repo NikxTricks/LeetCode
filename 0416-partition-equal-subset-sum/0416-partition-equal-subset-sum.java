51620c71e7148b710d38fc6286f8736f8769d943
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-09-17 00:32:51 -0400

    Time: 17 ms (95.19%), Space: 42 MB (88.81%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int total = 0;
+        for (int num: nums) {
+            total += num;
+        }
+        if (total % 2 == 1) {
+            return false;
+        }
+        boolean[] dp = new boolean[total/2 + 1];
+        dp[0] = true;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = dp.length - 1; j >= 0; j--) {
+                if (dp[j] && j + nums[i] < dp.length) {
+                    dp[j + nums[i]] = true;
+                }
+            }
+        }
+        
+//         for (boolean res: dp) {
+//             System.out.println(res);
+//         }
+        return dp[dp.length - 1];
+    }
+}
