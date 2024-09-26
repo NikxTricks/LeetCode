e77dffb9cf9331bcb1e3c83bdeb892fc06b3b64c
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-09-26 00:20:51 -0400

    Time: 43 ms (85.14%), Space: 111.2 MB (93.76%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,45 +1,117 @@
+class Node {
+    public int key;
+    public int val;
+    public Node next;
+    public Node prev;
+    
+    public Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+    }
+}
+
+class DoublyLinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
+    private int count;
+    
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        size = 0;
+    }
+    
+    public void add(Node node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+            node.next = node;
+            node.prev = node;
+        }
+        else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+            
+            head.prev = tail;
+            node.next = head;
+        }
+        size++;
+    }
+    
+    public Node remove(Node node) {
+        // System.out.printf("Removing for %d time\n", count);
+        // count++;
+        if (head.equals(tail)) {
+            head = null;
+            tail = null;
+            return node;
+        }
+        
+        if (tail.equals(node)) {
+            tail = node.prev;
+        }
+        else if (head.equals(node)) {
+            head = node.next;
+        }
+        
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        
+        node.next = null;
+        node.prev = null;
+        
+        size--;
+        
+        
+        return node;
+    }
+}
+
 class LRUCache {
     private int capacity;
-    private List<Integer> order;
-    private int[] data;
+    private DoublyLinkedList order;
+    private Map<Integer, Node> data;
     
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.order = new ArrayList<Integer>();
-        this.data = new int[(int) Math.pow(10, 4) + 1];
-        Arrays.fill(data, -1);
+        this.order = new DoublyLinkedList();
+        this.data = new HashMap<Integer, Node>();
     }
     
     public int get(int key) {
-        int out = data[key];
-        if (out != -1) {
-            order.add(removeItem(key)); //add item back into LRU ordering if present
+        Node cur = data.getOrDefault(key, null);
+        if (cur != null) {
+            //System.out.println(cur.val);
+            order.add(order.remove(cur)); //add item back into LRU ordering if present
+            return cur.val;
         }
-        return out;
+        else {
+            return -1;
+        }
     }
     
     public void put(int key, int value) {
-        if (data[key] != -1) {
-            data[key] = value;
-            order.add(removeItem(key));
+        Node cur = data.getOrDefault(key, null);
+        if (cur != null) {
+            cur.val = value;
+            order.add(order.remove(cur));
         }
         else {
-            if (order.size() >= this.capacity) {
-                data[order.remove(0)] = -1;
+            if (order.size >= this.capacity) {
+                data.remove(order.head.key);
+                order.remove(order.head);
             }
-            data[key] = value;
-            order.add(key); 
+            Node out = new Node(key, value);
+            order.add(out); 
+            data.put(key, out); //iffy
+            //System.out.println(out.next == null);
+            
         }
         
     }
     
-    public int removeItem(int item) {
-        int i = 0;
-        while (this.order.get(i) != item) {
-            i++;
-        }
-        return this.order.remove(i);
-    }
 }
 
 /**
