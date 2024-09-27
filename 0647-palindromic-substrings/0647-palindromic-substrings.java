af57e110446f6dc3fcf6b916643297c6016de485
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-09-26 23:01:01 -0400

    Time: 14 ms (47.23%), Space: 49.4 MB (6.71%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,15 +1,16 @@
 class Solution {
     public int countSubstrings(String s) {
-        int count = 0;
-        int m = s.length();
-        boolean[][] dp = new boolean[m][m];
-        for (int i = 0; i < m; i++) {
-            dp[i][i] = true;
-            count++;
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        int count = n;
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = 1;
             for (int j = 0; j < i; j++) {
-                if (s.charAt(i) == s.charAt(j) && (i - j <= 2 || dp[j + 1][i - 1])) {
-                    dp[j][i] = true;
-                    count++;
+                if (s.charAt(j) == s.charAt(i)) {
+                    if (i - j <= 2 || dp[j + 1][i - 1] == 1) {
+                        dp[j][i] = 1;
+                        count++;
+                    }
                 }
             }
         }
