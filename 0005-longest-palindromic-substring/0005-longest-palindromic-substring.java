85c983b05480fc2cc2668781526797be8c922549
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-09-27 00:00:01 -0400

    Time: 15 ms (88.53%), Space: 41.7 MB (99.04%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,26 +4,44 @@ class Solution {
         int max = 1;
         int start = 0;
         int end = 1;
-        boolean[][] dp = new boolean[m][m];
-        for (int i = 0; i < m; i++) {
-            dp[i][i] = true;
-            for (int j = 0; j < i; j++) {
-                if (s.charAt(j) == s.charAt(i)) {
-                    if (i - j <= 2 || dp[j + 1][i - 1]) {
-                        dp[j][i] = true;
-                        if (i - j + 1 > max) {
-                            max = i - j + 1;
-                            start = j;
-                            end = i + 1;
-                        }
-                    }
-                }
+        for (int cur = 0; cur < m; cur++) {
+            int i = cur;
+            int j = cur;
+            while (i >= 0 && j < m && s.charAt(i) == s.charAt(j)) {
+                i--;
+                j++;
             }
+            
+            if (j - i - 1 > max) {
+                max = j - i - 1;
+                start = i + 1;
+                end = j;
+            }
+            
+            i = cur; 
+            j = cur + 1;
+            while (i >= 0 && j < m && s.charAt(i) == s.charAt(j)) {
+                i--;
+                j++;
+            }
+            
+            if (j - i - 1 > max) {
+                max = j - i - 1;
+                start = i + 1;
+                end = j;
+            }
+            
         }
-        
+        //                         for (int w = 0; w < m; w++) {
+        //     for (int d = 0; d < m; d++) {
+        //         System.out.print(dp[w][d] + " ");
+        //     }
+        //     System.out.println("");
+        // }
         return s.substring(start, end);
         
         
         
     }
 }
+
