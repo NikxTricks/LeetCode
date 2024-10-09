865e4d161e0e120091038a48c90dc4c017913be0
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-10-09 17:12:02 -0400

    Time: 0 ms (100.00%), Space: 15.9 MB (74.91%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        std::vector<int> dp(n);
+        //std::vector<int> dp(n);
+        int prev = 0;
         
         for (int i = 1; i < n; i++) {
-            if (prices[i - 1] >= prices[i]) {
-                dp[i] = dp[i - 1];
-            }
-            else {
-                dp[i] = dp[i - 1] + (prices[i] - prices[i - 1]);
+            if (prices[i - 1] < prices[i]) {
+                prev += (prices[i] - prices[i - 1]);
             }
         }
         
-        return dp[n - 1];
+        return prev;
     }
 };
