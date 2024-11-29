072e495a468d3e0d043825cb469944f9aeeba01e
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-11-29 18:41:16 -0500

    Time: 32 ms (48.59%), Space: 55.7 MB (10.12%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,29 +1,24 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<double[]> order = new PriorityQueue<double[]>((a, b) -> {
-            return Double.compare(b[2], a[2]);
+        PriorityQueue<Pair> order = new PriorityQueue<Pair>((a, b) -> {
+            return Double.compare(b.dist, a.dist);
         });
         
         for (int[] point: points) {
-            double[] entry = new double[3];
-            entry[0] = point[0];
-            entry[1] = point[1];
-            entry[2] = Math.sqrt(point[0] * point[0] + point[1] * point[1]);
+            Pair entry = new Pair(point, Math.sqrt(point[0] * point[0] + point[1] * point[1]));
             if (order.size() < k) {
                 order.add(entry);
             }
-            else if (order.peek()[2] > entry[2]) {
+            else if (order.peek().dist > entry.dist) {
                 order.remove();
                 order.add(entry);
             }
+            
         }
-        
         int[][] out = new int[order.size()][2];
         int i = 0;
         while (!order.isEmpty()) {
-            double[] entry = order.remove();
-            out[i][0] = (int) entry[0];
-            out[i][1] = (int) entry[1];
+            out[i] = order.remove().point;
             i++;
         }
         
@@ -35,3 +30,13 @@ class Solution {
 }
 
 
+class Pair {
+    int[] point;
+    double dist;
+    
+    
+    public Pair(int[] point, double dist) {
+        this.point = point;
+        this.dist = dist;
+    }
+}
