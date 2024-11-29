4ec068f1595cf973ad6869cb8ab6bc60d14b3186
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-11-29 17:49:53 -0500

    Time: 0 ms (100.00%), Space: 42.5 MB (10.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,8 +1,5 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] out = new int[m + n];
-        
-        
         int i = m;
         int j = n;
         
