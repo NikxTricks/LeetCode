29352bb9f9b700fba5b3f14bed4772f59ed32400
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-11-28 22:45:31 -0500

    Time: 8 ms (19.39%), Space: 44.5 MB (71.07%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -22,7 +22,11 @@ class Solution {
         
         boolean left = helper(cur.left, p, q);
         boolean right = helper(cur.right, p, q);
-        if ((left && right) || (cur.val == p || cur.val == q) && (left || right) && lca == null) {
+        
+        if (left && right && lca == null) {
+            lca = cur;
+        }
+        else if ((cur.val == p || cur.val == q) && (left || right) && lca == null) {
             lca = cur;
         }
         
