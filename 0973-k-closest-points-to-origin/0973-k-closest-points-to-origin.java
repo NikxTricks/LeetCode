3cb239439e31baf71c075135a40d0dcf9e50fb24
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-11-29 18:02:59 -0500

    Time: 35 ms (33.33%), Space: 53 MB (50.22%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<double[]> order = new PriorityQueue<double[]>((a, b) -> {
+            return Double.compare(b[2], a[2]);
+        });
+        
+        for (int[] point: points) {
+            double[] entry = new double[3];
+            entry[0] = point[0];
+            entry[1] = point[1];
+            entry[2] = Math.sqrt(point[0] * point[0] + point[1] * point[1]);
+            if (order.size() < k) {
+                order.add(entry);
+            }
+            else if (order.peek()[2] > entry[2]) {
+                order.remove();
+                order.add(entry);
+            }
+        }
+        
+        int[][] out = new int[order.size()][2];
+        int i = 0;
+        while (!order.isEmpty()) {
+            double[] entry = order.remove();
+            out[i][0] = (int) entry[0];
+            out[i][1] = (int) entry[1];
+            i++;
+        }
+        
+        return out;
+        
+        
+    }
+    
+}
+
+
+class Pair {
+    int[] point;
+    int dist;
+    
+    
+    public Pair(int[] point, int dist) {
+        this.point = this.point;
+        this.dist = dist;
+    }
+}
