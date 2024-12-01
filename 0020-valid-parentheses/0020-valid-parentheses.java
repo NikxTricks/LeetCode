e2d860bb4538d7e96bb1a7ff46ed1b6fd1d87843
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-01 01:03:38 -0500

    Time: 2 ms (97.47%), Space: 42 MB (12.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,10 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> order = new Stack<>();
-        Set<Character> opens = new HashSet<>();
-        opens.add('(');
-        opens.add('{');
-        opens.add('[');
-        
-        Set<Character> closes = new HashSet<>();
-        closes.add(')');
-        closes.add('}');
-        closes.add(']');
         
         char[] chars = s.toCharArray();
         for (char c: chars) {
-            if (opens.contains(c)) {
+            if (c == '(' || c == '{' || c == '[') {
                 order.add(c);
             }
             else if (c == ')' && (order.isEmpty() || order.pop() != '(')) {
