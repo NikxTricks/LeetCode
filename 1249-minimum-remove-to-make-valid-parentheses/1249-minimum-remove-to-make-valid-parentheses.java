33b01f3265172dc086486ef1d552f51cfbba18d3
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-04 17:45:07 -0500

    Time: 19 ms (65.58%), Space: 45.1 MB (86.91%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -2,17 +2,13 @@ class Solution {
     public String minRemoveToMakeValid(String s) {
         StringBuilder out = new StringBuilder(s);
         Stack<Integer> order = new Stack<>();
-        Set<Integer> removal = new HashSet<>();
         
-        //int n = s.length();
-        //System.out.println(n);
         for (int i = 0; i < out.length(); i++) {
             if (out.charAt(i) == '(') {
                 order.push(i);
             }
             else if (out.charAt(i) == ')') {
                 if (order.isEmpty()) {
-                    //System.out.println(out.charAt(i));
                     out.deleteCharAt(i);
                     i--;
                 }
@@ -21,8 +17,7 @@ class Solution {
                 }
             }
         }
-        
-        //StringBuilder out = new StringBuilder(s);
+
         while (!order.isEmpty()) {
             out.deleteCharAt(order.pop());
         }
