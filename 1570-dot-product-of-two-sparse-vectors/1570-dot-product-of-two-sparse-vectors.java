e1b066ca88141d7a38ebac606b46e7bd1e1a3368
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-04 21:24:27 -0500

    Time: 2 ms (100.00%), Space: 56.5 MB (94.99%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
@@ -1,26 +1,15 @@
 class SparseVector {
     
     int[] nums;
-    Set<Integer> idx;
     SparseVector(int[] nums) {
         this.nums = nums;
-        this.idx = new HashSet<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                idx.add(i);
-            }
-        }
     }
     
 	// Return the dotProduct of two sparse vectors
     public int dotProduct(SparseVector vec) {
         int out = 0;
         for (int i = 0; i < this.nums.length; i++) {
-            if (vec.idx.contains(i) || this.idx.contains(i)) {
-                continue;
-            }
-            out += this.nums[i] * vec.nums[i];
+            out += (this.nums[i] == 0 || vec.nums[i] == 0) ? 0 : this.nums[i] * vec.nums[i];
         }
         
         return out;
