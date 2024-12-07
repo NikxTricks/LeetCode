358255700517d9d5f380371868d2711c7e999b24
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-07 13:07:53 -0500

    Time: 3 ms (98.58%), Space: 45.8 MB (23.17%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -1,29 +1,23 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
         List<int[]> temp = new ArrayList<>();
-        int i = 0;
-        int j = 0;
+        int i = 0, j = 0;
         
-        int[] cur = new int[2];
         while (i < firstList.length && j < secondList.length) {
-            if (firstList[i][0] > secondList[j][1]) {
-                j++;
-            }
-            else if (secondList[j][0] > firstList[i][1]) {
-                i++;
-            }
-            else {
+            // If there is an intersection
+            if (firstList[i][0] <= secondList[j][1] && secondList[j][0] <= firstList[i][1]) {
+                int[] cur = new int[2];
                 cur[0] = Math.max(firstList[i][0], secondList[j][0]);
                 cur[1] = Math.min(firstList[i][1], secondList[j][1]);
                 temp.add(cur);
-                cur = new int[2];
-                if (firstList[i][1] > secondList[j][1]) {
-                    j++;
-                }
-                else {
-                    i++;
-                }
-            } 
+            }
+            
+            // Move the pointer for the interval that ends first
+            if (firstList[i][1] < secondList[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
         }
         
         return temp.toArray(new int[temp.size()][]);
