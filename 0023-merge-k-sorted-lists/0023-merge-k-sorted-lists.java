895f436a15691bc9c0da4eb9ba0556f0dd31f948
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-07 01:58:11 -0500

    Time: 1 ms (99.89%), Space: 45.8 MB (5.17%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -23,60 +23,33 @@ class Solution {
         ListNode i = helper(lists, start, (start + end) / 2);
         ListNode j = helper(lists, ((start + end) / 2) + 1, end);
         
-        ListNode out = null;
-        ListNode head = null;
+        
+        ListNode head = new ListNode(0);
+        ListNode out = head;
         while (i != null && j != null) {
-            //System.out.println(i.val);
-            //System.out.println(j.val);
             if (i.val > j.val) {
-                if (out == null) {
-                    out = new ListNode(j.val);
-                    head = out;
-                }
-                else {
-                    out.next = new ListNode(j.val);
-                    out = out.next;
-                }
+                out.next = new ListNode(j.val);
+                out = out.next;
                 j = j.next;
             }
             else {
-                if (out == null) {
-                    out = new ListNode(i.val);
-                    head = out;
-                }
-                else {
-                    out.next = new ListNode(i.val);
-                    out = out.next;
-                }
+                out.next = new ListNode(i.val);
+                out = out.next;
                 i = i.next;
             }
-            //System.out.println(out.val);
         }
         while (i != null) {
-            if (out == null) {
-                out = new ListNode(i.val);
-                head = out;
-            }
-            else {
-                out.next = new ListNode(i.val);
-                out = out.next; 
-            }
+            out.next = new ListNode(i.val);
+            out = out.next; 
             i = i.next;  
         }
         while (j != null) {
-            if (out == null) {
-                out = new ListNode(j.val);
-                head = out;
-            }
-            else {
-                out.next = new ListNode(j.val);
-                out = out.next; 
-            }
+            out.next = new ListNode(j.val);
+            out = out.next; 
             j = j.next;  
         }
-        //out.next = null; //remove any loops
         
-        return head;
+        return head.next;
     }
     
 }
