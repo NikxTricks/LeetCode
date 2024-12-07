f02286178cc3db35d80f9b66b8d99907327f5844
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-07 03:03:39 -0500

    Time: 13 ms (70.37%), Space: 42.5 MB (56.90%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> out = new ArrayList<>();
+        Map<TreeNode, TreeNode> parents = new HashMap<>();
+        Queue<TreeNode> order = new LinkedList<>();
+        Set<TreeNode> visited = new HashSet<>();
+        
+        order.add(root);
+        
+        while (!order.isEmpty()) {
+            int size = order.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = order.remove();
+                //System.out.println(cur.val);
+                if (cur.left != null) {
+                    parents.put(cur.left, cur);
+                    order.add(cur.left);
+                }
+                if (cur.right != null) {
+                    parents.put(cur.right, cur);
+                    order.add(cur.right);
+                }
+            }
+        }
+        
+        order.add(target);
+        visited.add(target);
+        int count = 0;
+    
+        while (!order.isEmpty()) {
+            int size = order.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = order.remove();
+                if (count == k) {
+                    out.add(cur.val);
+                }
+//                 if (count ) {
+                    
+//                 }
+                if (cur.left != null && !visited.contains(cur.left)) {
+                    order.add(cur.left);
+                    visited.add(cur.left);
+                }
+                if (cur.right != null && !visited.contains(cur.right)) {
+                    order.add(cur.right);
+                    visited.add(cur.right);
+                }
+                TreeNode parent = parents.getOrDefault(cur, null);
+                if (parent != null && !visited.contains(parent)) {
+                    order.add(parent);
+                    visited.add(parent);
+                }
+    
+            }
+            count++;
+        }
+        
+        
+        
+        return out;
+    }
+    
+}
