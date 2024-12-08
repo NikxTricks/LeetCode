290e649af1db2f6d32c8866ba2244e882534d343
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-08 18:39:20 -0500

    Time: 4 ms (22.59%), Space: 42.1 MB (10.48%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,19 +1,27 @@
 class Solution {
     public String customSortString(String order, String s) {
-        Character[] chars = new Character[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            chars[i] = s.charAt(i);
+        int[] pos = new int[26];
+        List<Character> temp = new ArrayList<>();
+        for (int i = 0; i < order.length(); i++) {
+            pos[order.charAt(i) - 'a'] = i;
         }
-        Arrays.sort(chars, (a, b) -> order.indexOf(a) - order.indexOf(b)); 
+        for (int i = 0; i < s.length(); i++) {
+            temp.add(s.charAt(i));
+        }
         
+        Collections.sort(temp, (a, b) -> {
+            return pos[a - 'a'] - pos[b - 'a'];
+        });
         
         StringBuilder out = new StringBuilder();
-        for (Character c: chars) {
+        
+        for (char c: temp) {
             out.append(c);
         }
         
         return out.toString();
-        }
+    }
+    
         
         
 }
