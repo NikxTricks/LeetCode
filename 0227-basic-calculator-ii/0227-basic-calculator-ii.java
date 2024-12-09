ccdbb8b98ae22ed631800096e52d3dc9ef37be71
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-08 22:18:58 -0500

    Time: 21 ms (57.20%), Space: 46.4 MB (39.33%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,49 +1,41 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> order = new Stack<>();
+        Stack<Integer> nums = new Stack<>();
         
-        
-        int cur = 0;
-        char op = '+';
-        
-        for (int i = 0; i < s.length(); i++) {
+        int i = 0;
+        int num = 0;
+        char prev = '+';
+        while (i < s.length()) {
             char c = s.charAt(i);
-
             if (Character.isDigit(c)) {
-                cur *= 10;
-                cur += (c - '0');
+                num *= 10;
+                num += (c - '0');
             }
-            if (isOp(c) || i == s.length() - 1) {
-                switch (op) {
-                    case '+':
-                        order.push(cur);
-                        break;
-                    case '-':
-                        order.push(cur * -1);
-                        break;
-                    case '*':
-                        order.push(order.pop() * cur);
-                        break;
-                    case '/':
-                        order.push(order.pop() / cur);
-                        break;
+            if (c == '+' || c == '-' || c == '*' || c == '/' || i == s.length() - 1) {
+                if (prev == '+') {
+                    nums.push(num);
                 }
-
-                op = c;
-                cur = 0;
+                else if (prev == '-') {
+                    nums.push(num * -1);
+                }
+                else if (prev == '*') {
+                    nums.push(nums.pop() * num);
+                }
+                else {
+                    nums.push(nums.pop() / num);     
+                }
+                prev = c;
+                num = 0;
             }
+            i++;
         }
         
-        int ans = 0;
-        while (!order.isEmpty()) {
-            ans += order.pop();
+        int out = 0;
+        while (!nums.isEmpty()) {
+            out += nums.pop();
         }
- 
-        return ans;
+        
+        return out;
     }
     
-    
-    private boolean isOp(char c) {
-        return c == '+' || c == '-' || c == '/' || c == '*';
-    }
 }
