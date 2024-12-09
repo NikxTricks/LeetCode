af3f66066f59af69cd88cce7869a11c38ebecfb4
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-09 13:28:09 -0500

    Time: 8 ms (77.48%), Space: 57.4 MB (64.82%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
@@ -14,21 +14,39 @@ class SparseVector {
         int j = 0;
         
         int sum = 0;
-        while (i < vals.size() && j < vec.vals.size()) {
-            if (vals.get(i)[0] > vec.vals.get(j)[0]) {
-                j++;
+        while (i < vals.size()) {
+            //System.out.println(vals.get(i)[0]);
+            int idx = idx = find(vec.vals, vals.get(i)[0]);
+            if (idx != -1) {
+                
+                sum += (vals.get(i)[1] * vec.vals.get(idx)[1]);
             }
-            else if (vals.get(i)[0] < vec.vals.get(j)[0]) {
-                i++;
-            }
-            else {
-                sum += vals.get(i)[1] * vec.vals.get(j)[1];
-                i++;
-                j++;
-            }
+            i++;
         }
         
         return sum;
+    }
+    
+    private int find(List<int[]> vals, int target) {
+        int s = 0;
+        int e = vals.size() - 1;
+        
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            //System.out.println(mid);
+            if (vals.get(mid)[0] == target) {
+                return mid;
+            }
+            if (vals.get(mid)[0] > target) {
+                e = mid - 1;
+            }
+            else {
+                s = mid + 1;
+            }
+        }
+        
+        
+        return -1;
     }
 }
 
