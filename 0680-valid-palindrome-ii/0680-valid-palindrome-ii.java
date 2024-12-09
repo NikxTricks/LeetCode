afd6bb618b6e402766f9e67c706f3123fc57f1d6
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-08 22:36:38 -0500

    Time: 4 ms (99.71%), Space: 45.3 MB (68.78%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,40 +1,31 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int n = s.length();
+            
+        int i = 0;
+        int j = s.length() - 1;
         
-        int f = 0;
-        int b = n - 1;
-        
-        while (f < b) {
-            if (s.charAt(f) == s.charAt(b)) {
-                f++;
-                b--;
-            }
-            else {
-                return helper(s, f + 1, b) || helper(s, f, b - 1);
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return helper(s, i + 1, j) || helper(s, i, j - 1);
             }
             
+            i++;
+            j--;
         }
         
         return true;
     }
     
-    
-    public boolean helper(String s, int i, int j) {        
-        int f = i;
-        int b = j;
-        
-        while (f < b) {
-            if (s.charAt(f) == s.charAt(b)) {
-                f++;
-                b--;
-            }
-            else {
+    private boolean helper(String word, int s, int e) {
+        while (s < e) {
+            if (word.charAt(s) != word.charAt(e)) {
                 return false;
             }
-            
+            s++;
+            e--;
         }
         
         return true;
     }
+    
 }
