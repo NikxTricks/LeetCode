a2908ddff6e6c9c40183701412423855f5a9bec8
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-08 21:38:07 -0500

    Time: 0 ms (100.00%), Space: 43.4 MB (5.54%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -8,17 +8,13 @@ class Solution {
             exp *= -1;
         }
         
-        
-        double base = x;
         double out = 1;
         while (exp > 0) {
             if (exp % 2 == 1) {
                 out *= x;
-               // exp--;
             }
                 x *= x;
                 exp /= 2;
-            //System.out.printf("%f ,%f, %f\n", out, odds, x);
         }
         
         return out;
