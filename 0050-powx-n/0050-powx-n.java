01524c0633e120564452d73130e102607d0b795e
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-08 21:49:36 -0500

    Time: 0 ms (100.00%), Space: 43.3 MB (8.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,25 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-      double odds = 1;
-        long exp = n;
-        
-        if (n < 0) {
-            x = 1/x;
-            exp *= -1;
-        }
-        
         double out = 1;
+
+        long exp = n;
+
+        if (n < 0) {
+           x = 1/x;
+           exp *= -1; 
+        }
+    
+        
         while (exp > 0) {
             if (exp % 2 == 1) {
                 out *= x;
             }
-                x *= x;
-                exp /= 2;
+            
+            x *= x;
+            exp /= 2;
         }
         
         return out;
-        
-        
-        
     }
 }
