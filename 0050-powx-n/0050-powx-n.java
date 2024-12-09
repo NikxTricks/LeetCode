6291294650d2ee94cd754f89f57c66c2154ef93c
Nimai Patel <101423716+NikxTricks@users.noreply.github.com>
2024-12-09 11:37:38 -0500

    Time: 0 ms (100.00%), Space: 42.3 MB (48.06%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,17 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
         double out = 1;
-
+        
         long exp = n;
-
+        
         if (n < 0) {
-           x = 1/x;
-           exp *= -1; 
+            x = 1/x;
+            exp *= -1;
         }
-    
         
         while (exp > 0) {
-            if (exp % 2 == 1) {
+            if (exp % 2 == 1) { //2^(k + 1)
                 out *= x;
             }
             
@@ -20,5 +19,7 @@ class Solution {
         }
         
         return out;
+        
+        
     }
 }
